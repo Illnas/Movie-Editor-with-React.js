abdc004d7a48dc158a372723b743be1806596e3f
programer10 <programer10@ucione.local>
2022-02-14 20:27:35 +0100

    Got functionality of adding image working

--- src/Components/CreateMovies.js
+++ src/Components/CreateMovies.js
@@ -1,13 +1,104 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 
 const CreateMovies = () => {
-  const [selectedFile, setSelectedFile] = useState();
+  const [imgData, setImgData] = useState([]);
   const inputFile = useRef(null);
+  const [data, setData] = useState([]);
+  const [textInput, setTextInput] = useState([])
+  const [isSending, setIsSending] = useState(false);
+  const isMounted = useRef(true);
 
-  const onButtonClick = () => {
-    // `current` points to the mounted file input element
-    inputFile.current.click();
+  const inputFileRef = useRef(null);
+
+  const onFileChange = (e) => {
+    /*Selected files data can be collected here.*/
+    if (e.target.files[0]) {
+
+      console.log(e.target.files[0]);
+      setData(e.target.files[0]);
+      console.log(typeof data)
+      setTextInput({"name": "Wall-E", "publicationYear": 2008})
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        setImgData(reader.result);
+      });
+      reader.readAsDataURL(e.target.files[0]);
+    }
   };
+  const onBtnClick = () => {
+    /*Collecting node-element and performing click*/
+    inputFileRef.current.click();
+  };
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const sendRequest = useCallback(async () => {
+    // don't send again while we are sending
+    if (isSending) return;
+    var formdata = new FormData();
+    formdata.append("data", "");
+    formdata.append("files.poster", data, "jpeg");
+
+    const requestOptions = {
+      method: "POST",
+      body: formdata,
+      headers: { Authorization: `Bearer ${localStorage.token}` },
+      redirect: "follow",
+    };
+
+    // update state
+    setIsSending(true);
+    // send the actual request
+
+    try {
+      fetch(
+        "https://zm-movies-assignment.herokuapp.com/api/movies",
+        requestOptions
+      )
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log("error", error);
+    }
+
+    // once the request is sent, update state again
+    if (isMounted.current)
+      // only update if we are still mounted
+      setIsSending(false);
+  }, [isSending]); // update the callback if the state changes
+
+  /*   useEffect(() => {
+      const fetchData = async () => {
+        var formdata = new FormData();
+        formdata.append("data", "");
+        formdata.append("files.poster", inputFile.files[0], "file");
+
+        const requestOptions = {
+          method: "GET",
+          headers: { Authorization: `Bearer ${localStorage.token}` },
+          redirect: "follow",
+        };
+
+        try {
+          setLoading(true);
+          const response = await fetch(
+            "https://zm-movies-assignment.herokuapp.com/api/auth/local",
+            requestOptions
+          );
+          const json = await response.json();
+          localStorage.setItem("token", json.jwt)
+          setLoading(false);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+      fetchData();
+    }, []); */
 
   return (
     <section className="creation">
@@ -18,8 +109,14 @@ const CreateMovies = () => {
       </header>
 
       <div className="creation-container">
-        <form>
-          <div className="image-file" onClick={onButtonClick}>
+        <form action="#">
+          <div className="image-file" type="file" onClick={onBtnClick}>
+            <img
+              src={imgData}
+              alt="Girl in a jacket"
+              width="500"
+              height="600"
+            />
             <svg
               width="16"
               height="16"
@@ -36,7 +133,8 @@ const CreateMovies = () => {
             <input
               type="file"
               id="file"
-              ref={inputFile}
+              ref={inputFileRef}
+              onChange={onFileChange}
               style={{ display: "none" }}
             />
           </div>
@@ -52,7 +150,9 @@ const CreateMovies = () => {
             </div>
             <div className="btns">
               <button type="">Clear</button>
-              <button type="submit">Submit</button>
+              <button type="submit" disabled={isSending} onClick={sendRequest}>
+                Submit
+              </button>
             </div>
           </div>
         </form>
